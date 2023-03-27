f96118f9345e532064d83068d7fc36ff98a278b8
louzanizineddine <louzanizineddine@gmail.com>
2023-03-27 16:45:15 -0700

    dealing with null pointer

--- _rot13.c
+++ _rot13.c
@@ -1,32 +1,23 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * leet- output function
+ * rot13 - rotate a string 13th times
  *
- * @s: the strin to encodes
+ * @str: the strin to encodes
  *
  * Description:'Write a function that encodes a string into 1337.
- * Letters a and A should be replaced by 4
- * Letters e and E should be replaced by 3
- * Letters o and O should be replaced by 0
- * Letters t and T should be replaced by 7
- * Letters l and L should be replaced by 1
- *
- * You can only use one if in your code
- * You can only use two loops in your code
- * You are not allowed to use switch
- * You are not allowed to use any ternary operation'
- *
- * Return:  the pointer to coded strng .
+  * Return:  the pointer to coded strng .
  */
+
 char *rot13(char *str)
 {
 	char *s = str;
 	char *rot13 = malloc(sizeof(char) * _strlen(str) + 1);
 	int i;
 
-	if (rot13 == NULL)
-		return (NULL);
+	if (str == NULL)
+		return ("(AHYY)");
 
 	for (i = 0; *s != '\0'; s++, i++)
 	{
